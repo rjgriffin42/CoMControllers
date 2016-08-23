ce86d5ae9ec7b9524864cb8b913facb71c44d351
Robert <rgriffin@ihmc.us>
2016-08-23 15:35:41 -0400

    started adding a one step test

--- test/ihmc/us/comControllers/footstepOptimization/ICPAdjustmentSolverTest.java
+++ test/ihmc/us/comControllers/footstepOptimization/ICPAdjustmentSolverTest.java
@@ -2,12 +2,17 @@ package ihmc.us.comControllers.footstepOptimization;
 
 import org.junit.Assert;
 import org.junit.Test;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.geometry.FramePoint2d;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
 
 public class ICPAdjustmentSolverTest extends ICPAdjustmentSolver
 {
    private static final double epsilon = 0.00001;
    private static final int maxNumberOfFootstepsToConsider = 5;
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
 
    public ICPAdjustmentSolverTest()
    {
@@ -22,26 +27,26 @@ public class ICPAdjustmentSolverTest extends ICPAdjustmentSolver
       boolean includeFeedback = false;
       boolean useTwoCMPs = false;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       checkDimensions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       includeFeedback = true;
       useTwoCMPs = false;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       includeFeedback = false;
       useTwoCMPs = true;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       includeFeedback = true;
       useTwoCMPs = true;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
 
@@ -50,23 +55,65 @@ public class ICPAdjustmentSolverTest extends ICPAdjustmentSolver
       includeFeedback = false;
       useTwoCMPs = false;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(maxNumberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(maxNumberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       includeFeedback = false;
       useTwoCMPs = true;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(maxNumberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
 
       includeFeedback = true;
       useTwoCMPs = true;
 
-      setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
       checkDimensions(maxNumberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+   }
+
+   @DeployableTestMethod(estimatedDuration = 1.0)
+   @Test(timeout = 21000)
+   public void testOneStep()
+   {
+      YoVariableRegistry registry = new YoVariableRegistry("robert");
+      DoubleYoVariable omega = new DoubleYoVariable("omega", registry);
+      DoubleYoVariable yoTime = new DoubleYoVariable("t", registry);
+      omega.set(3.0);
+
+      int numberOfFootstepsToConsider = 1;
+      boolean includeFeedback = false;
+      boolean useTwoCMPs = false;
+
+      TargetTouchdownICPCalculator targetTouchdownICPCalculator = new TargetTouchdownICPCalculator(omega, registry);
+      StepRecursionMultiplierCalculator stepRecursionMultiplierCalculator = new StepRecursionMultiplierCalculator(omega, maxNumberOfFootstepsToConsider, registry);
+
+      FramePoint2d perfectCMP = new FramePoint2d(worldFrame);
+      FramePoint2d currentICP = new FramePoint2d(worldFrame);
+      FramePoint2d desiredFootstep1 = new FramePoint2d(worldFrame);
+      FramePoint2d desiredFootstep2 = new FramePoint2d(worldFrame);
+
+      double stepLength = 0.2;
+      double stanceWidth = 0.1;
+
+      perfectCMP.set(0.0, -stanceWidth); // right foot stance
+      currentICP.set(0.1, 0.0);
+      desiredFootstep1.set(stepLength, stanceWidth);
+      desiredFootstep2.set(2 * stepLength, -stanceWidth);
+
+      double
+
+      double singleSupportDuration = 2.0;
+      double doubleSupportDuration = 1.0;
+      double remainingTime = 1.0;
+
+      super.setProblemConditions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
+
+      super.reset();
+
+      checkDimensions(numberOfFootstepsToConsider, includeFeedback, useTwoCMPs);
    }
 
    private void checkDimensions(int nummberOFFootstepsToConsider, boolean includeFeedback, boolean useTwoCMPs)
