4ff54ce922a610e08522d58c0f329a973a917272
Robert <rjgriffin42@gmail.com>
2016-08-26 13:00:01 -0400

    hopefully finished the icp optimization solver

---
+++ src/us/ihmc/comControllers/icpOptimization/ICPOptimizationController.java
@@ -0,0 +1,8 @@
+package us.ihmc.comControllers.icpOptimization;
+
+/**
+ * Created by robert on 8/26/16.
+ */
+public class ICPOptimizationController
+{
+}
--- src/us/ihmc/comControllers/icpOptimization/ICPOptimizationSolver.java
+++ src/us/ihmc/comControllers/icpOptimization/ICPOptimizationSolver.java
@@ -1,15 +1,31 @@
 package us.ihmc.comControllers.icpOptimization;
 
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.interfaces.linsol.LinearSolver;
+import org.ejml.ops.CommonOps;
+import us.ihmc.robotics.geometry.FramePoint2d;
+import us.ihmc.robotics.geometry.FrameVector2d;
 import us.ihmc.robotics.linearAlgebra.MatrixTools;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+
+import java.util.ArrayList;
 
 public class ICPOptimizationSolver
 {
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+
    protected final DenseMatrix64F solverInput_G;
    protected final DenseMatrix64F solverInput_g;
 
    protected final DenseMatrix64F solverInput_H;
    protected final DenseMatrix64F solverInput_h;
+
+   protected final DenseMatrix64F footstepCost_H;
+   protected final DenseMatrix64F footstepCost_h;
+
+   protected final DenseMatrix64F feedbackCost_H;
+   protected final DenseMatrix64F feedbackCost_h;
 
    protected final DenseMatrix64F solverInput_Aeq;
    protected final DenseMatrix64F solverInput_AeqTrans;
@@ -18,12 +34,38 @@ public class ICPOptimizationSolver
    protected final DenseMatrix64F dynamics_Aeq;
    protected final DenseMatrix64F dynamics_beq;
 
+   protected final ArrayList<DenseMatrix64F> footstepRecursionMutlipliers = new ArrayList<>();
+   protected final ArrayList<DenseMatrix64F> referenceFootstepLocations = new ArrayList<>();
+
+   protected final DenseMatrix64F finalICPRecursion = new DenseMatrix64F(2, 1);
+   protected final DenseMatrix64F cmpOffsetRecursionEffect = new DenseMatrix64F(2, 1);
+   protected final DenseMatrix64F currentICP = new DenseMatrix64F(2, 1);
+   protected final DenseMatrix64F perfectCMP = new DenseMatrix64F(2, 1);
+   protected double omega;
+   protected double timeRemaining;
+
+   protected final ArrayList<DenseMatrix64F> footstepWeights = new ArrayList<>();
+   protected final DenseMatrix64F feedbackWeight = new DenseMatrix64F(2, 2);
+   protected final DenseMatrix64F feedbackGain = new DenseMatrix64F(2, 2);
+
+   private final LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.linear(0);
+
+   protected final DenseMatrix64F solution;
+   protected final DenseMatrix64F lagrangeMultiplierSolution;
+   protected final DenseMatrix64F footstepLocationSolution;
+   protected final DenseMatrix64F feedbackDeltaSolution;
+   protected final DenseMatrix64F feedbackLocation;
+
    protected final int maximumNumberOfFootstepsToConsider;
 
    protected int numberOfFootstepsToConsider;
    protected int numberOfFreeVariables = 0;
    protected int numberOfFootstepVariables = 0;
    protected int numberOfLagrangeMultipliers = 2;
+
+   private boolean useFeedback = false;
+   private boolean useStepAdjustment = true;
+   private boolean useTwoCMPs = false;
 
    public ICPOptimizationSolver(ICPOptimizationParameters icpOptimizationParameters)
    {
@@ -39,20 +81,43 @@ public class ICPOptimizationSolver
       solverInput_H = new DenseMatrix64F(maximumNumberOfFreeVariables, maximumNumberOfFreeVariables);
       solverInput_h = new DenseMatrix64F(maximumNumberOfFreeVariables, 1);
 
+      footstepCost_H = new DenseMatrix64F(2 * maximumNumberOfFootstepsToConsider, 2 * maximumNumberOfFootstepsToConsider);
+      footstepCost_h = new DenseMatrix64F(2 * maximumNumberOfFootstepsToConsider, 1);
+
+      feedbackCost_H = new DenseMatrix64F(2, 2);
+      feedbackCost_h = new DenseMatrix64F(2, 1);
+
       solverInput_Aeq = new DenseMatrix64F(maximumNumberOfFreeVariables, maximumNumberOfLagrangeMultipliers);
       solverInput_AeqTrans = new DenseMatrix64F(maximumNumberOfLagrangeMultipliers, maximumNumberOfFreeVariables);
       solverInput_beq = new DenseMatrix64F(maximumNumberOfLagrangeMultipliers, 1);
 
       dynamics_Aeq = new DenseMatrix64F(maximumNumberOfFreeVariables, 2);
       dynamics_beq = new DenseMatrix64F(2, 1);
+
+      for (int i = 0; i < maximumNumberOfFootstepsToConsider; i++)
+      {
+         referenceFootstepLocations.add(new DenseMatrix64F(2, 1));
+         footstepRecursionMutlipliers.add(new DenseMatrix64F(2, 2));
+         footstepWeights.add(new DenseMatrix64F(2, 2));
+      }
+
+      solution = new DenseMatrix64F(maximumNumberOfFreeVariables + maximumNumberOfLagrangeMultipliers, 1);
+      lagrangeMultiplierSolution = new DenseMatrix64F(maximumNumberOfLagrangeMultipliers, 1);
+      footstepLocationSolution = new DenseMatrix64F(2 * maximumNumberOfFootstepsToConsider, 1);
+      feedbackDeltaSolution = new DenseMatrix64F(2, 1);
+      feedbackLocation = new DenseMatrix64F(2, 1);
    }
 
-   public void submitProblemConditions(int numberOfFootstepsToConsider, boolean useStepAdjustment, boolean useFeedback)
+   public void submitProblemConditions(int numberOfFootstepsToConsider, boolean useStepAdjustment, boolean useFeedback, boolean useTwoCMPs)
    {
       if (!useFeedback && !useStepAdjustment)
       {
          throw new RuntimeException("No possible feedback mechanism available.");
       }
+
+      this.useFeedback = useFeedback;
+      this.useStepAdjustment = useStepAdjustment;
+      this.useTwoCMPs = useTwoCMPs;
 
       if (useFeedback && !useStepAdjustment)
          this.numberOfFootstepsToConsider = 0;
@@ -61,9 +126,11 @@ public class ICPOptimizationSolver
 
       numberOfFootstepVariables = 2 * this.numberOfFootstepsToConsider;
       numberOfFreeVariables = numberOfFootstepsToConsider + 2;
+
+      reset();
    }
 
-   public void reset()
+   private void reset()
    {
       int size = numberOfFreeVariables + numberOfLagrangeMultipliers;
       solverInput_G.zero();
@@ -75,6 +142,14 @@ public class ICPOptimizationSolver
       solverInput_h.zero();
       solverInput_H.reshape(numberOfFreeVariables, numberOfFreeVariables);
       solverInput_h.reshape(numberOfFreeVariables, 1);
+
+      footstepCost_H.zero();
+      footstepCost_h.zero();
+      footstepCost_H.reshape(numberOfFootstepVariables, numberOfFootstepVariables);
+      footstepCost_h.reshape(numberOfFootstepVariables, 1);
+
+      feedbackCost_H.zero();
+      feedbackCost_h.zero();
 
       solverInput_Aeq.zero();
       solverInput_AeqTrans.zero();
@@ -88,24 +163,258 @@ public class ICPOptimizationSolver
       solverInput_Aeq.reshape(numberOfFreeVariables, 2);
       solverInput_AeqTrans.reshape(2, numberOfFreeVariables);
       solverInput_beq.reshape(2, 1);
+
+      for (int i = 0; i < maximumNumberOfFootstepsToConsider; i++)
+      {
+         referenceFootstepLocations.get(i).zero();
+         footstepRecursionMutlipliers.get(i).zero();
+         footstepWeights.get(i).zero();
+      }
+
+      finalICPRecursion.zero();
+      cmpOffsetRecursionEffect.zero();
+      currentICP.zero();
+      perfectCMP.zero();
+
+      feedbackWeight.zero();
+      feedbackGain.zero();
+
+      solution.zero();
+      lagrangeMultiplierSolution.zero();
+      footstepLocationSolution.zero();
+      feedbackDeltaSolution.zero();
+      feedbackLocation.zero();
+      solution.reshape(numberOfFreeVariables + numberOfLagrangeMultipliers, 1);
+      lagrangeMultiplierSolution.reshape(numberOfLagrangeMultipliers, 1);
+      footstepLocationSolution.reshape(numberOfFootstepVariables, 1);
    }
 
-   public compute()
+   private final DenseMatrix64F identity = CommonOps.identity(2, 2);
+
+   public void setFootstepAdjustmentConditions(int footstepIndex, double recursionMultiplier, double weight, FramePoint2d referenceFootstepLocation)
    {
+      setFootstepRecursionMutliplier(footstepIndex, recursionMultiplier);
+      setFootstepWeight(footstepIndex, weight);
+      setReferenceFootstepLocation(footstepIndex, referenceFootstepLocation);
+   }
+
+   private void setFootstepRecursionMutliplier(int footstepIndex, double recursionMultiplier)
+   {
+      CommonOps.setIdentity(identity);
+      MatrixTools.addMatrixBlock(footstepRecursionMutlipliers.get(footstepIndex), 0, 0, identity, 0, 0, 2, 2, recursionMultiplier);
+   }
+
+   private void setFootstepWeight(int footstepIndex, double weight)
+   {
+      CommonOps.setIdentity(identity);
+      MatrixTools.addMatrixBlock(footstepWeights.get(footstepIndex), 0, 0, identity, 0, 0, 2, 2, weight);
+   }
+
+   private void setReferenceFootstepLocation(int footstepIndex, FramePoint2d referenceFootstepLocation)
+   {
+      referenceFootstepLocation.changeFrame(worldFrame);
+      referenceFootstepLocations.get(footstepIndex).set(0, 0, referenceFootstepLocation.getX());
+      referenceFootstepLocations.get(footstepIndex).set(1, 0, referenceFootstepLocation.getY());
+   }
+
+   private void setFeedbackConditions(double feedbackWeight, double feedbackGain)
+   {
+      CommonOps.setIdentity(this.feedbackWeight);
+      CommonOps.setIdentity(this.feedbackGain);
+
+      CommonOps.scale(feedbackWeight, this.feedbackWeight);
+      CommonOps.scale(feedbackGain, this.feedbackGain);
+   }
+
+   public void compute(FramePoint2d finalICPRecursion, FramePoint2d cmpOffsetRecursionEffect, FramePoint2d currentICP, FramePoint2d perfectCMP,
+                       double omega, double timeRemaining)
+   {
+      finalICPRecursion.changeFrame(worldFrame);
+      cmpOffsetRecursionEffect.changeFrame(worldFrame);
+      currentICP.changeFrame(worldFrame);
+      perfectCMP.changeFrame(worldFrame);
+
+      this.finalICPRecursion.set(0, 0, finalICPRecursion.getX());
+      this.finalICPRecursion.set(1, 0, finalICPRecursion.getY());
+
+      this.currentICP.set(0, 0, currentICP.getX());
+      this.currentICP.set(1, 0, currentICP.getY());
+
+      this.perfectCMP.set(0, 0, perfectCMP.getX());
+      this.perfectCMP.set(1, 0, perfectCMP.getY());
+
+      this.timeRemaining = timeRemaining;
+      this.omega = omega;
+
+      if (useTwoCMPs)
+      {
+         this.cmpOffsetRecursionEffect.set(0, 0, cmpOffsetRecursionEffect.getX());
+         this.cmpOffsetRecursionEffect.set(1, 0, cmpOffsetRecursionEffect.getY());
+      }
+
+      if (useFeedback)
+         addFeedbackTask();
+
+      if (useStepAdjustment)
+         addStepAdjustmentTask();
+
+      addDynamicConstraint();
+
       assembleTotalProblem();
+
+      solve(solution);
+
+      extractLagrangeMultiplierSolution(lagrangeMultiplierSolution);
+      if (useStepAdjustment)
+         extractFootstepSolutions(footstepLocationSolution);
+      if (useFeedback)
+         extractFeedbackDeltaSolution(feedbackDeltaSolution);
+
+      computeFeedbackLocation();
+
+      computeCostToGo();
+   }
+
+
+   private void addFeedbackTask()
+   {
+      MatrixTools.addMatrixBlock(feedbackCost_H, 0, 0, feedbackWeight, 0, 0, 2, 2, 1.0);
+      feedbackCost_h.zero();
+
+      MatrixTools.addMatrixBlock(solverInput_H, numberOfFootstepVariables, numberOfFootstepVariables, feedbackCost_H, 0, 0, 2, 2, 1.0);
+      MatrixTools.addMatrixBlock(solverInput_h, numberOfFootstepVariables, 0, feedbackCost_h, 0, 0, 2, 1, 1.0);
+   }
+
+   private final DenseMatrix64F tmpFootstepObjective = new DenseMatrix64F(2, 1);
+   private void addStepAdjustmentTask()
+   {
+      for (int i = 0; i < numberOfFootstepsToConsider; i++)
+      {
+         MatrixTools.addMatrixBlock(footstepCost_H, 2 * i, 2 * i, footstepWeights.get(i), 0, 0, 2, 2, 1.0);
+
+         tmpFootstepObjective.zero();
+         tmpFootstepObjective.set(referenceFootstepLocations.get(i));
+         CommonOps.mult(footstepWeights.get(i), tmpFootstepObjective, tmpFootstepObjective);
+
+         MatrixTools.addMatrixBlock(footstepCost_h, 2 * i, 0, tmpFootstepObjective, 0, 0, 2, 1, 1.0);
+      }
+
+      MatrixTools.addMatrixBlock(solverInput_H, 0, 0, footstepCost_H, 0, 0, numberOfFootstepVariables, numberOfFootstepVariables, 1.0);
+      MatrixTools.addMatrixBlock(solverInput_h, 0, 0, footstepCost_h, 0, 0, numberOfFootstepVariables, 1, 1.0);
+   }
+
+   private void addDynamicConstraint()
+   {
+      computeDynamicConstraint();
+
+      MatrixTools.addMatrixBlock(solverInput_Aeq, 0, 0, dynamics_Aeq, 0, 0, numberOfFreeVariables, 2, 1.0);
+      MatrixTools.addMatrixBlock(solverInput_beq, 0, 0, dynamics_beq, 0, 0, numberOfFreeVariables, 1, 1.0);
+   }
+
+   private void computeDynamicConstraint()
+   {
+      double currentStateProjection = Math.exp(omega * timeRemaining);
+
+      if (useFeedback)
+         addFeedbackToDynamicConstraint(currentStateProjection);
+      if (useStepAdjustment)
+         addFootstepRecursionsToDynamicConstraint();
+
+      CommonOps.scale(currentStateProjection, currentICP);
+      CommonOps.scale((1.0 - currentStateProjection), perfectCMP);
+
+      CommonOps.subtractEquals(currentICP, finalICPRecursion);
+      CommonOps.subtractEquals(currentICP, perfectCMP);
+
+      if (useTwoCMPs)
+         CommonOps.subtractEquals(currentICP, cmpOffsetRecursionEffect);
+
+      MatrixTools.addMatrixBlock(dynamics_beq, 0, 0, currentICP, 0, 0, 2, 1, 1.0);
+   }
+
+   private void addFeedbackToDynamicConstraint(double currentStateProjection)
+   {
+      CommonOps.invert(feedbackGain);
+      CommonOps.scale(currentStateProjection, feedbackGain);
+
+      MatrixTools.addMatrixBlock(dynamics_Aeq, numberOfFootstepVariables, 0, feedbackGain, 0, 0, 2, 2, 1.0);
+   }
+
+   private void addFootstepRecursionsToDynamicConstraint()
+   {
+      for (int i = 0; i < numberOfFootstepsToConsider; i++)
+      {
+         MatrixTools.addMatrixBlock(dynamics_Aeq, 2 * i, 0, footstepRecursionMutlipliers.get(i), 0, 0, 2, 2, 1.0);
+      }
    }
 
    private void assembleTotalProblem()
    {
+      // add matrix minimization objective
       MatrixTools.setMatrixBlock(solverInput_G, 0, 0, solverInput_H, 0, 0, numberOfFreeVariables, numberOfFreeVariables, 1.0);
       MatrixTools.setMatrixBlock(solverInput_g, 0, 0, solverInput_h, 0, 0, numberOfFreeVariables, 1, 1.0);
 
+      // add constraints
       MatrixTools.setMatrixBlock(solverInput_G, 0, numberOfFreeVariables, solverInput_Aeq, 0, 0, numberOfFreeVariables, numberOfLagrangeMultipliers, 1.0);
       MatrixTools.setMatrixBlock(solverInput_G, numberOfFreeVariables, 0, solverInput_AeqTrans, 0, 0, numberOfLagrangeMultipliers, numberOfFreeVariables, 1.0);
       MatrixTools.setMatrixBlock(solverInput_g, numberOfLagrangeMultipliers, 0, solverInput_beq, 0, 0, numberOfLagrangeMultipliers, 1, 1.0);
    }
 
-   private void solve()
+   private void solve(DenseMatrix64F solutionToPack)
    {
+      solver.setA(solverInput_G);
+      solver.solve(solverInput_g, solutionToPack);
+   }
+
+   private void extractFootstepSolutions(DenseMatrix64F footstepLocationSolutionToPack)
+   {
+      MatrixTools.setMatrixBlock(footstepLocationSolutionToPack, 0, 0, solution, 0, 0, numberOfFootstepVariables, 1, 1.0);
+   }
+
+   private void extractFeedbackDeltaSolution(DenseMatrix64F feedbackSolutionToPack)
+   {
+      MatrixTools.setMatrixBlock(feedbackSolutionToPack, 0, 0, solution, numberOfFootstepsToConsider, 0, 2, 1, 1.0);
+   }
+
+   private void extractLagrangeMultiplierSolution(DenseMatrix64F lagrangeMultiplierSolutionToPack)
+   {
+      MatrixTools.setMatrixBlock(lagrangeMultiplierSolutionToPack, 0, 0, solution, numberOfFreeVariables, 0, numberOfLagrangeMultipliers, 1, 1.0);
+   }
+
+   private void computeFeedbackLocation()
+   {
+      feedbackLocation.set(perfectCMP);
+      CommonOps.addEquals(feedbackLocation, feedbackDeltaSolution);
+   }
+
+   private void computeCostToGo()
+   {
+      //todo
+   }
+
+   public void getFootstepSolutionLocation(int footstepIndex, FramePoint2d footstepLocationToPack)
+   {
+      footstepLocationToPack.setToZero(worldFrame);
+      footstepLocationToPack.setX(footstepLocationSolution.get(2 * footstepIndex, 0));
+      footstepLocationToPack.setY(footstepLocationSolution.get(2 * footstepIndex + 1, 0));
+   }
+
+   public void getCMPFeedbackDifference(FrameVector2d cmpFeedbackDifferenceToPack)
+   {
+      cmpFeedbackDifferenceToPack.setToZero(worldFrame);
+      cmpFeedbackDifferenceToPack.setX(feedbackDeltaSolution.get(0, 0));
+      cmpFeedbackDifferenceToPack.setY(feedbackDeltaSolution.get(1, 0));
+   }
+
+   public void getCMPFeedback(FramePoint2d cmpFeedbackToPack)
+   {
+      cmpFeedbackToPack.setToZero(worldFrame);
+      cmpFeedbackToPack.setX(feedbackLocation.get(0, 0));
+      cmpFeedbackToPack.setY(feedbackLocation.get(1, 0));
+   }
+
+   public double getCostToGo()
+   {
+      return 0.0; // todo
    }
 }
