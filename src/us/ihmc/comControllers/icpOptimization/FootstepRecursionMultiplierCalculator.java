264e19cfe462aa6df88345818e537e73b45c5ab8
Robert <rgriffin@ihmc.us>
2016-08-31 20:20:38 -0400

    finished pushing all the projection multipliers into their own classes

--- src/us/ihmc/comControllers/icpOptimization/FootstepRecursionMultiplierCalculator.java
+++ src/us/ihmc/comControllers/icpOptimization/FootstepRecursionMultiplierCalculator.java
@@ -1,9 +1,6 @@
 package us.ihmc.comControllers.icpOptimization;
 
-import us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers.CMPRecursionMultipliers;
-import us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers.FinalICPRecursionMultiplier;
-import us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers.RemainingStanceCMPProjectionMultipliers;
-import us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers.StanceCMPProjectionMultipliers;
+import us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers.*;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
@@ -17,25 +14,17 @@ public class FootstepRecursionMultiplierCalculator
    private final ArrayList<DoubleYoVariable> doubleSupportDurations = new ArrayList<>();
    private final ArrayList<DoubleYoVariable> singleSupportDurations = new ArrayList<>();
 
-   private final DoubleYoVariable currentStateProjectionMultiplier;
-
-   private final DoubleYoVariable exitCMPDurationInPercentOfStepTime;
-   private final DoubleYoVariable doubleSupportSplitFraction;
-   private final DoubleYoVariable omega;
-
    private final FinalICPRecursionMultiplier finalICPRecursionMultiplier;
    private final CMPRecursionMultipliers cmpRecursionMultipliers;
    private final StanceCMPProjectionMultipliers stanceCMPProjectionMultipliers;
    private final RemainingStanceCMPProjectionMultipliers remainingStanceCMPProjectionMultipliers;
+   private final CurrentStateProjectionMultiplier currentStateProjectionMultiplier;
 
    private final int maxNumberOfFootstepsToConsider;
 
    public FootstepRecursionMultiplierCalculator(DoubleYoVariable exitCMPDurationInPercentOfStepTime, DoubleYoVariable doubleSupportSplitFraction,
          DoubleYoVariable omega, BooleanYoVariable isInTransfer, BooleanYoVariable isInTransferEntry, BooleanYoVariable useTwoCMPs, int maxNumberOfFootstepsToConsider, YoVariableRegistry parentRegistry)
    {
-      this.exitCMPDurationInPercentOfStepTime = exitCMPDurationInPercentOfStepTime;
-      this.doubleSupportSplitFraction = doubleSupportSplitFraction;
-      this.omega = omega;
       this.maxNumberOfFootstepsToConsider = maxNumberOfFootstepsToConsider;
 
       for (int i = 0; i < maxNumberOfFootstepsToConsider; i++)
@@ -50,7 +39,7 @@ public class FootstepRecursionMultiplierCalculator
       remainingStanceCMPProjectionMultipliers = new RemainingStanceCMPProjectionMultipliers("", omega, doubleSupportSplitFraction, exitCMPDurationInPercentOfStepTime,
             useTwoCMPs, isInTransfer, isInTransferEntry, registry);
 
-      currentStateProjectionMultiplier = new DoubleYoVariable("currentSTateProjectionMultiplier", registry);
+      currentStateProjectionMultiplier = new CurrentStateProjectionMultiplier(registry, omega);
 
       finalICPRecursionMultiplier = new FinalICPRecursionMultiplier(registry, omega, doubleSupportSplitFraction, isInTransfer);
 
@@ -92,14 +81,8 @@ public class FootstepRecursionMultiplierCalculator
 
    public void computeRemainingProjectionMultipliers(double timeRemaining, boolean useTwoCMPs, boolean isInTransfer, boolean isInTransferEntry)
    {
-      computeCurrentStateProjection(timeRemaining);
-
+      currentStateProjectionMultiplier.compute(timeRemaining);
       remainingStanceCMPProjectionMultipliers.compute(timeRemaining, doubleSupportDurations, singleSupportDurations);
-   }
-
-   private void computeCurrentStateProjection(double timeRemaining)
-   {
-      currentStateProjectionMultiplier.set(Math.exp(omega.getDoubleValue() * timeRemaining));
    }
 
    public double getCMPRecursionExitMultiplier(int footstepIndex)
---
+++ src/us/ihmc/comControllers/icpOptimization/projectionAndRecursionMultipliers/CurrentStateProjectionMultiplier.java
@@ -0,0 +1,31 @@
+package us.ihmc.comControllers.icpOptimization.projectionAndRecursionMultipliers;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+
+import java.util.ArrayList;
+
+public class CurrentStateProjectionMultiplier extends DoubleYoVariable
+{
+   private static final String name = "CurrentStateProjectionMultiplier";
+
+   private final DoubleYoVariable omega;
+
+   public CurrentStateProjectionMultiplier(YoVariableRegistry registry, DoubleYoVariable omega)
+   {
+      this("", registry, omega);
+   }
+
+   public CurrentStateProjectionMultiplier(String namePrefix, YoVariableRegistry registry, DoubleYoVariable omega)
+   {
+      super(namePrefix + name, registry);
+
+      this.omega = omega;
+   }
+
+   public void compute(double timeRemaining)
+   {
+      this.set(Math.exp(omega.getDoubleValue() * timeRemaining));
+   }
+}
